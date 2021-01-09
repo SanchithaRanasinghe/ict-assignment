9f8ae0c586430182278f75a3e3464905a848e14c
SanchithaRanasinghe <76244685+SanchithaRanasinghe@users.noreply.github.com>
2021-01-09 14:50:12 +0530

    ict assignment 01 Q10
    Write a C program to concatenate two strings: first name and last name. Display the full name as the output.

---
+++ ict q10.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+int Even(int a);
+int main(){
+	char name1[]="CHAMOD ";  //name 1 is first name 
+	char name2[]="SANCHITHA"; //name 2 is last name
+	
+	
+	strcat(name1,name2); //concatenate  first name and last name
+	puts(name1);
+	return 0;
+	
+	}
+
+
